59b4a71443627f9f3dc3a3c639bcfb94e88fbdc0
NyelNyel <tadicjaineil@gmail.com>
2021-11-02 20:53:36 +0800

    master

--- src/camera-test.py
+++ src/camera-test.py
@@ -12,10 +12,18 @@ while(True):
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
     for(x, y, w, h) in faces:
         print(x,y,w,h)
+        roi_gray = gray[y:y+h, x:x+w]
+        img_item = "my-image.png"
+        cv2.imwrite(img_item, roi_gray)
+
+        color = (255, 0, 0)
+        stroke = 2
+        end_cord_x = x + w
+        end_cord_y = y + h
+        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
 
     # Display the resulting frame
     cv2.imshow('frame',frame)
-    cv2.imshow('gray',gray)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
